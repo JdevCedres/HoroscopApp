ec620eb3727adf02cae2dff74706897d74dfba2e
JoseDev <jdevmov@gmail.com>
2023-09-22 17:40:12 +0100

    ApiService daggerHilt provides Repository

---
+++ app/src/main/java/com/example/horoscapp/domain/usecase/GetPredictionUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.horoscapp.domain.usecase
+
+import com.example.horoscapp.domain.Repository
+import javax.inject.Inject
+
+class GetPredictionUseCase@Inject constructor(repository: Repository) {
+}
